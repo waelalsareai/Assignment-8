aa4a4597b6eca370b399399ed5ee2b5b14dcf3c6
A Student <student@nowhere.edu>
2025-11-22 18:34:18 +0000

    Nov 22, 2025, 10:34 AM

--- postNetFunctions.cpp
+++ postNetFunctions.cpp
@@ -22,7 +22,15 @@ string encodeDigit(char digit) {
       case '1': return "00011";
       case '2': return "00101";
       case '3': return "00110";
-      case '4': return ""
+      case '4': return "01001";
+      case '5': return "01010";
+      case '6': return "01100";
+      case '7': return "10001";
+      case '8': return "10010";
+      case '9': return "10100";
+      default:
+      throw std::logic_error("invalid digit for PostNET");
+    }
 }
 
 string encodeZip(const string& zip) {
