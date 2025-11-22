929d20ed8ffd684f6454d13ef80577c5fbbcf6a1
A Student <student@nowhere.edu>
2025-11-22 18:08:55 +0000

    Nov 22, 2025, 10:08 AM

--- postNetFunctions.cpp
+++ postNetFunctions.cpp
@@ -3,3 +3,14 @@
 using std::string;
 
 // --- Function definitions will go here ---
+string parseState(const string& line) {
+    return "";
+}
+
+string parseCity(const string& line) {
+    return "";
+}
+
+string parseZip(const string& line) {
+    return "";
+}
